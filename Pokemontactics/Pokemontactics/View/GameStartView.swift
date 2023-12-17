56357e1a34b2244b2b0499a4613ccffb9d34da16
shintyakos <shintyakos1145@gmail.com>
2023-12-17 22:24:40 +0900

    ゲーム開始画面作成

--- Pokemontactics/Pokemontactics/View/GameStartView.swift
+++ Pokemontactics/Pokemontactics/View/GameStartView.swift
@@ -15,6 +15,8 @@ struct GameStartView: View {
     @State var deckName2 = ""
     @State var limitTime = "75"
     
+    @State var selectedTab = 1
+    
     private var safeareaInsets: UIEdgeInsets? {
         let scenes = UIApplication.shared.connectedScenes
         let windowScene = scenes.first as? UIWindowScene
@@ -24,48 +26,7 @@ struct GameStartView: View {
     var body: some View {
         ZStack {
             VStack(alignment: .leading, spacing: 0) {
-                Spacer()
-                HStack(alignment: .top, spacing: 0) {
-                    VStack(alignment: .center, spacing: 0) {
-                        Spacer().frame(height: 13)
-                        Text("シングル戦")
-                          .font(
-                            Font.custom("Roboto", size: 14)
-                              .weight(.bold)
-                          )
-                          .multilineTextAlignment(.center)
-                          .foregroundColor(.white)
-                        Spacer().frame(height: 16)
-                        Divider()
-                            .frame(maxWidth: .infinity, maxHeight: 2)
-                            .background(.white)
-                    }
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 48)
-                    .background(Color(red: 0.94, green: 0.5, blue: 0.19))
-                    VStack {
-                        Spacer().frame(height: 13)
-                        Text("マッチ戦")
-                          .font(Font.custom("Roboto", size: 14))
-                          .multilineTextAlignment(.center)
-                          .foregroundColor(Color(red: 0.2, green: 0.23, blue: 0.25).opacity(0.39))
-                        Spacer().frame(height: 16)
-                        Divider()
-                            .frame(maxWidth: .infinity, maxHeight: 2)
-                            .background(Color(red: 0.94, green: 0.5, blue: 0.19))
-                    }
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 48)
-                    .background(Color(red: 1, green: 0.85, blue: 0.73))
-                }
-                .padding(0)
-                .frame(maxWidth: .infinity)
-                .frame(height: 48)
-                .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
-                .overlay(
-                    Rectangle()
-                        .stroke(Color(red: 0.55, green: 0.27, blue: 0.07), lineWidth: 1)
-                )
+                TabBarView(selectedTab: $selectedTab)
                 VStack(alignment: .center, spacing: 27) {
                     ZStack {
                         VStack(alignment: .center, spacing: 10) {
@@ -192,6 +153,60 @@ struct GameStartView: View {
         .background(Color(red: 0.95, green: 0.9, blue: 0.67))
         .navigationBarTitle("ゲーム開始")
         .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct TabBarView : View {
+    @Binding var selectedTab : Int
+    var body: some View {
+        Spacer()
+        HStack(alignment: .top, spacing: 0) {
+            VStack(alignment: .center, spacing: 0) {
+                Spacer().frame(height: 13)
+                Text("シングル戦")
+                  .font(
+                    Font.custom("Roboto", size: 14)
+                      .weight(.bold)
+                  )
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(.white)
+                Spacer().frame(height: 16)
+                Divider()
+                    .frame(maxWidth: .infinity, maxHeight: 2)
+                    .background(.white)
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 48)
+            .background(selectedTab == 1 ? Color(red: 0.94, green: 0.5, blue: 0.19) : Color.gray)
+            .onTapGesture {
+                self.selectedTab = 1
+            }
+            VStack {
+                Spacer().frame(height: 13)
+                Text("マッチ戦")
+                  .font(Font.custom("Roboto", size: 14))
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(Color(red: 0.2, green: 0.23, blue: 0.25).opacity(0.39))
+                Spacer().frame(height: 16)
+                Divider()
+                    .frame(maxWidth: .infinity, maxHeight: 2)
+                    .background(Color(red: 0.94, green: 0.5, blue: 0.19))
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 48)
+            .background(Color(red: 1, green: 0.85, blue: 0.73))
+            .onTapGesture {
+                self.selectedTab = 2
+            }
+        }
+        .padding(0)
+        .frame(maxWidth: .infinity)
+        .frame(height: 48)
+        .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
+        .overlay(
+            Rectangle()
+                .stroke(Color(red: 0.55, green: 0.27, blue: 0.07), lineWidth: 1)
+        )
     }
 }
 
